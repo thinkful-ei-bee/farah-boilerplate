3fc42b908f481afe94bdd08a2c3fe91a5cf32d89
Farah Student <farah.gus@hotmail.com>
2019-04-03 14:33:51 -0400

    added boilerplate

--- src/app.js
+++ src/app.js
@@ -3,15 +3,31 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const { NOTE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (process.env.NODE_ENV === 'production')
+const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
 app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next){
+    let response 
+    if (NODE.ENV === "production"){
+        response = { error: {message: 'server error'}}
+    } else {
+        console.log(error)
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response)
+})
 
 module.exports = app
