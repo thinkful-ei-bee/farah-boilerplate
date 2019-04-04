7592e0511e6b55e710d5ecbe177937ef8ee22232
Farah Student <farah.gus@hotmail.com>
2019-04-04 14:24:09 -0400

    fixed typo

--- src/app.js
+++ src/app.js
@@ -1,33 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NOTE_ENV } = require('./config')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const {NOTE_ENV} = require("./config");
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === "production"
+  ? "tiny"
+  : "common";
 
-app.use(morgan(morganOption))
-app.use(cors())
-app.use(helmet())
+app.use(morgan(morganOption));
+app.use(cors());
+app.use(helmet());
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = {
+      error: {
+        message: "server error"
+      }
+    };
+  } else {
+    console.error(error);
+    response = {
+      message: error.message,
+      error
+    };
+  }
+  res.status(500).json(response);
+});
 
-app.use(function errorHandler(error, req, res, next){
-    let response 
-    if (NODE.ENV === "production"){
-        response = { error: {message: 'server error'}}
-    } else {
-        console.log(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
-
-module.exports = app
+module.exports = app;
