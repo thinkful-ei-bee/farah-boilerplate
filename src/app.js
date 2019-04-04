46d28e6fe6672bb6cafc222498e22e07ee59e254
Farah Student <farah.gus@hotmail.com>
2019-04-04 14:50:57 -0400

    moved helmet middleware before cors

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ const morganOption = NODE_ENV === "production"
   : "common";
 
 app.use(morgan(morganOption));
-app.use(cors());
 app.use(helmet());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
